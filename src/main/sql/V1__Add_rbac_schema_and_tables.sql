b5bd9c412843ba9f85fc54cbe28a4fb33d94696b
raeesapps <raees.aamir@yandex.com>
2022-10-22 16:21:19 +0100

    Write RBAC DDL queries

---
+++ src/main/sql/V1__Add_rbac_schema_and_tables.sql
@@ -0,0 +1,18 @@
+CREATE SCHEMA rbac;
+
+CREATE TABLE rbac.users (id uuid PRIMARY KEY);
+
+CREATE TABLE rbac.roles (
+    id uuid PRIMARY KEY,
+    rName text,
+    resources text[],
+    verbs text[]
+);
+
+CREATE TABLE rbac.bindings (
+    userId uuid,
+    roleId uuid,
+    PRIMARY KEY (userId, roleId),
+    CONSTRAINT userId_id FOREIGN KEY (userId) REFERENCES users (id),
+    CONSTRAINT roleId_id FOREIGN KEY (roleId) REFERENCES roles (id)
+);
