fd4365e4182c259b61372e1baf798561423b5fca
raeesapps <raees.aamir@yandex.com>
2022-10-22 19:59:02 +0100

    Remove redundant logger

--- src/main/java/net/raees/RbacService.java
+++ src/main/java/net/raees/RbacService.java
@@ -7,11 +7,9 @@ import net.raees.permissions.User;
 import java.sql.Connection;
 import java.sql.SQLException;
 import java.util.UUID;
-import java.util.logging.Logger;
 import java.util.stream.IntStream;
 
 public class RbacService {
-    private static final Logger logger = Logger.getLogger(RbacService.class.getName());
     private final RbacDataStore dataStore;
 
     RbacService(RbacDataStore dataStore) {
